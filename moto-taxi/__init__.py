7c821d165f353443e97bb5cec45502a67c5d0d82
BOB MAKTUB <atendimento.tecnomundos@gmail.com>
2025-04-21 08:26:46 -0300

    Add moto-taxi/__init__.py 1745234806513

---
+++ moto-taxi/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module initializes the moto-taxi package.
+Contains core functionality and configuration for the application.
+"""
+
+
+def dummy_function():
+    """A dummy function to ensure the module is recognized."""
+    return True
